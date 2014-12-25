7554871d3d8b02116c085bdef10eb824100c599f
Ed Scheinerman <ers@jhu.edu>
2014-12-24 20:35:04 -0500

    convert_simple now works for SimpleDigraphs

--- src/simple_converters.jl
+++ src/simple_converters.jl
@@ -1,7 +1,7 @@
 # These are functions to convert SimpleGraph's to formats in Julia's
 # Graphs module.
 
-# Convert a graph into a "simple_graph". 
+# Convert a graph or digraph into a "simple_graph".
 #
 # We return a triple: the simple_graph H with vertex set 1:n, a
 # dictionary d mapping the vertices of G to the vertices of H, and the
@@ -10,8 +10,11 @@
 import Graphs.simple_graph, Graphs.add_edge!
 export convert_simple
 
-function convert_simple{T}(G::SimpleGraph{T})
+function convert_simple(G::AbstractSimpleGraph)
+    T = vertex_type(G)
     n = NV(G)
+    has_dir = isa(G,SimpleDigraph)
+
 
     d = vertex2idx(G)
     dinv = Dict{Int,T}()
@@ -20,11 +23,17 @@ function convert_simple{T}(G::SimpleGraph{T})
         dinv[v] = k
     end
 
-    H = simple_graph(n,is_directed=false)
-    for e in G.E
+    H = simple_graph(n,is_directed=has_dir)
+
+    EE = elist(G)
+    for e in EE
         u = d[e[1]]
         v = d[e[2]]
         add_edge!(H,u,v)
     end
     return (H,d,dinv)
 end
+
+
+
+
